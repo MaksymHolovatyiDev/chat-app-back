6a4f6b544b68933b198a4e169370817d7afec979
MaksymHolovatyi <maksym.holovatyi.dev@gmail.com>
2023-09-28 17:52:30 +0300

    Rename folder

---
+++ src/app/Sockets/index.ts
@@ -0,0 +1,36 @@
+import {User} from 'models/user';
+import {Server} from 'socket.io';
+import {DefaultEventsMap} from 'socket.io/dist/typed-events';
+
+export class Sockets {
+  private static instance: Sockets;
+
+  constructor() {
+    if (!Sockets.instance) Sockets.instance = this;
+    return Sockets.instance;
+  }
+
+  init(io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>) {
+    io.use(async (socket: any, next: any) => {
+      const userId = socket.handshake.auth.userId;
+      if (!userId) {
+        return next(new Error('Invalid id!'));
+      }
+      const user = await User.findByIdAndUpdate(userId, {socketId: socket.id});
+      if (!user) {
+        return next(new Error('Invalid id!'));
+      }
+
+      (socket as any).userId = userId;
+      next();
+    });
+
+    io.on('connection', (socket: any) => {
+      console.log(`⚡: ${socket.id} user just connected!`);
+      socket.on('disconnect', async () => {
+        await User.findByIdAndUpdate(socket.userId, {socketId: null});
+        console.log('🔥: A user disconnected');
+      });
+    });
+  }
+}
--- src/infra/Tcp.ts
+++ src/infra/Tcp.ts
@@ -4,41 +4,7 @@ import {useExpressServer} from 'routing-controllers';
 import {createServer} from 'http';
 import {Server} from 'socket.io';
 import {controllers} from 'app/domain';
-import {User} from 'models/user';
-import {DefaultEventsMap} from 'socket.io/dist/typed-events';
-
-export class Sockets {
-  private static instance: Sockets;
-
-  constructor() {
-    if (!Sockets.instance) Sockets.instance = this;
-    return Sockets.instance;
-  }
-
-  init(io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>) {
-    io.use(async (socket: any, next: any) => {
-      const userId = socket.handshake.auth.userId;
-      if (!userId) {
-        return next(new Error('Invalid id!'));
-      }
-      const user = await User.findByIdAndUpdate(userId, {socketId: socket.id});
-      if (!user) {
-        return next(new Error('Invalid id!'));
-      }
-
-      (socket as any).userId = userId;
-      next();
-    });
-
-    io.on('connection', (socket: any) => {
-      console.log(`⚡: ${socket.id} user just connected!`);
-      socket.on('disconnect', async () => {
-        await User.findByIdAndUpdate(socket.userId, {socketId: null});
-        console.log('🔥: A user disconnected');
-      });
-    });
-  }
-}
+import {Sockets} from 'app/Sockets';
 
 const {PORT} = process.env;
 
